581867744573f0c778d382557f01b7642a384472
Karlp-Web <karlenportillo1@gmail.com>
2024-12-18 19:19:58 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void createDog(Dog inDog, String breed, String name, String color, double weight)
-{
+  
+  
+
+
+
+  //public static void createDog(Dog inDog, String breed, String name, String color, double weight)
+//{
  inDog.setBreed(breed);
  inDog.setName(name);
  inDog.setColor(color);
  inDog.setWeight(weight);
-}
+//}
 
 //First constructor ever made!!!!!!!
-  public static void main(String[] args)//takes the in all the values
-  {
+  //public static void main(String[] args)//takes the in all the values
+  //{
     Dog d = new Dog("Weiner dog", "Schnitzel", "red", 1000);
     Dog myDog = new Dog();
 
     Dog dog1 = new Dog(); //if dont make a constructor means its right and didnt write the code yet 
     Dog dog2 = new Dog("yomomma", "got", "my",5);                                                        
 
-  }
+  //}
 }
--- Plane.java
+++ Plane.java
@@ -1,4 +1,26 @@
 public class Plane
 {
-  // implement the Plane class here
+  //This sets location as an int
+  private int location;
+
+  //runs Plane() and gives a 0
+  Public Plane()
+  {
+    this.locaiton = 0;
+  }
+
+  Public Plane(int loc) //this runs Plane(int loc) 
+  {
+    if (loc >= -2000 && loc <= 2000) //checks it if the number is between these two numbers
+    {
+      this.location = loc; //sets location as loc (location becomes the input loc)
+    }
+   else
+   {
+    this.location = 0; //if not then location is = o 
+   }
+  }
+
+  
+
 }
