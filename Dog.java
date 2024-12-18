84131e2531ef1bcba839b528e415bd1aeb77be03
Karlp-Web <karlenportillo1@gmail.com>
2024-12-18 18:36:16 +0000

    yomomma

--- Dog.java
+++ Dog.java
@@ -15,30 +15,52 @@ public Dog() //the code is now right previous without any info because we made i
 
 public Dog(String iname, String iBreed, String icolor, double iweight)
 {
-  breed = iName;
-  name = iBreed;
+  breed = ibreed;
+  name = iname;
   color = iColor;
-  weight = iWeight;
-}
+  if(weight < 0)
+    {
+      weight = 0;
+    }
+    else
+      {
+        weight = iweight;
+      }
+ }
+
 
 public Dog(String iBreed, String iname, String icolor, double iweight)
-
+{
+  breed = ibreed;
+  name = iname;
+  color = icolor;
+  setWeight(iweight);
+}
   // Make setters
   public void setBreed(String iBreed)
   {
-    breed = iBreed;
+    this.breed = breed;
   }
-  
+
+Public void setName(String name) { _mame = name; }
+
+
+
+
+
   public void setweight(double iweight)
   {
     if(weight < 0)
     {
       weight = 0;
+    }  
+
+    else
       {
         weight = iweight
       }
-    }
-  } 
+  }
+
   
   public void setname(String iname)
   {
--- Student.java
+++ Student.java
@@ -4,6 +4,20 @@ public class Student
   private int grade;
   private String name;
 
+
+  public void setGrade(int grade)
+  {
+    this.grade = grade;
+  }
+
+  public void setID(int ID)
+  {
+    this.ID = ID;
+  }
+
+  public void setName (String name) { _name = name; }
+
+
   // Constructors
   public Student(int input_id, int input_grade, String input_name)
   {
@@ -11,6 +25,9 @@ public class Student
     grade = input_grade;
     name = input_name;
   }
+
+
+
 
   public String toString()
   {
